892395f4f77de5f9ccba847ff56e25a7ffefb3ee
Bytenol <ccosmos416@gmail.com>
2024-09-14 19:29:28 -0400

    updated readme.md

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-.
+# MetalBall Crush 3D
 
-https://bytenol.github.io/MetalBall-Crush-3D/
+Play online at: https://bytenol.github.io/MetalBall-Crush-3D/
+
+![Project Logo](assets/images/stack-ball-break-platform_banner.jpg "Logo Title")
+
+## How to Contribute
+Ensure to create an empty directory "emscripten_build" locally, this directory is where emscripten cmake cached files will be saved in. 
+The command to build the source has been integrated inside the nodeon.json file. 
+
+All you need to do is to run the following commands in seperate command shell
+
+
+```bash 
+npx vite
+npx nodemon
+```
+
+whenever the you make any changes to the .cpp, .json or .obj files, it automatically rebuild the project.
+
+### Note
+Make sure you have vite and nodemon installed. Open the project folder and run the command below to install any dependencies
+
+```bash
+npm install
--- StackBall3D.js
+++ StackBall3D.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpyoioee05.js
+// include: /tmp/tmp3r7s5k9o.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -222,21 +222,21 @@ Module['FS_createPath']("/assets", "shaders", true, true);
 
   })();
 
-// end include: /tmp/tmpyoioee05.js
-// include: /tmp/tmp8s94dqbq.js
+// end include: /tmp/tmp3r7s5k9o.js
+// include: /tmp/tmp8kz_8i8w.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp8s94dqbq.js
-// include: /tmp/tmp_70w42cn.js
+  // end include: /tmp/tmp8kz_8i8w.js
+// include: /tmp/tmpdt7tf1sb.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp_70w42cn.js
+  // end include: /tmp/tmpdt7tf1sb.js
 
 
 // Sometimes an existing Module object exists with properties
